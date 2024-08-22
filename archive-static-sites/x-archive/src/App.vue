fe4eb0827501c05f2ac033019a7b30d6ca624e2a
Micah Lee <micah@micahflee.com>
2024-08-21 17:43:08 -0700

    Make the static website put all its assets in an assets folder

--- archive-static-sites/x-archive/src/App.vue
+++ archive-static-sites/x-archive/src/App.vue
@@ -1,30 +1,43 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <template v-if="dbInitialized">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </template>
+  <template v-else>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+      <div class="container">
+        <div class="text-center">
+          <img src="./assets/logo.png" alt="Logo" class="img-fluid mb-4">
+        </div>
+        <div class="form-group select-sqlite">
+          <label for="dbFileInput" class="form-label mt-3">To view your X archive, select your <code>data.sqlite3</code>
+            file below</label>
+          <input type="file" class="form-control mt-3" id="dbFileInput">
+        </div>
+      </div>
+    </div>
+  </template>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.select-sqlite {
   text-align: center;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
+.select-sqlite label {
   font-weight: bold;
-  color: #2c3e50;
+  font-size: 1.5rem;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.select-sqlite input {
+  display: block;
+  margin: 0 auto;
+  max-width: 400px;
 }
 </style>
--- archive-static-sites/x-archive/src/views/HomeView.vue
+++ archive-static-sites/x-archive/src/views/HomeView.vue
@@ -4,11 +4,5 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-
-@Options({
-  components: {},
-})
-export default class HomeView extends Vue { }
+<script setup lang="ts">
 </script>
