f7c62e617843c9c4ef0af6c8cbfc9b139d7b4e8c
Micah Lee <micah@micahflee.com>
2024-10-02 11:24:19 -0700

    Fix package.json too, and no need to specify path to forge.config.ts

--- scripts/make-dev.sh
+++ scripts/make-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export SEMIPHEMERAL_ENV=dev
 export DEBUG=electron-packager,electron-universal,electron-forge*
-npx tsx ./node_modules/.bin/electron-forge make --config=forge.config.ts
+npx tsx ./node_modules/.bin/electron-forge make
--- scripts/make-prod.sh
+++ scripts/make-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export SEMIPHEMERAL_ENV=prod
 export DEBUG=electron-packager,electron-universal,electron-forge*
-npx tsx ./node_modules/.bin/electron-forge make --config=forge.config.ts --arch universal
+npx tsx ./node_modules/.bin/electron-forge make --arch universal
