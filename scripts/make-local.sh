5e73daa249c7db57646110f931a4acc3f55eac5e
Micah Lee <micah@micahflee.com>
2024-10-02 11:21:36 -0700

    Fix make scripts after upgrading deps

--- scripts/make-local.sh
+++ scripts/make-local.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export SEMIPHEMERAL_ENV=local
 export DEBUG=electron-packager,electron-universal,electron-forge*
-electron-forge make
+npx tsx ./node_modules/.bin/electron-forge make --config=forge.config.ts
