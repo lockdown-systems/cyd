bc244e432038daff7234c6e01eb5cff606728878
Micah Lee <micah@micahflee.com>
2024-10-24 19:51:26 -0700

    Fix linux build process

--- scripts/publish-dev-linux.sh
+++ scripts/publish-dev-linux.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
+# Cleanup from last time
+rm -r ./out-arm64
+rm -r ./out-amd64
+
 # Build release for arm64
 docker build --platform linux/arm64 -f Dockerfile-linux-release -t semiphemeral-linux-release .
 docker run --platform linux/arm64 --rm -v $(pwd):/app semiphemeral-linux-release npm run make-dev-linux
-cp -r out/ ./out-arm64
+mv ./out/ ./out-arm64
 
 # Build release for amd64
 docker build --platform linux/amd64 -f Dockerfile-linux-release -t semiphemeral-linux-release .
 docker run --platform linux/amd64 --rm -v $(pwd):/app semiphemeral-linux-release npm run make-dev-linux
-cp -r out/ ./out-amd64
-
-# Merge the two builds
-rm -rf out/
-mkdir -p out/make/deb out/make/rpm out/make/zip
-mv out-arm64/make/deb/* out/make/deb
-mv out-arm64/make/rpm/* out/make/rpm
-mv out-arm64/make/zip/* out/make/zip
-mv out-amd64/make/deb/* out/make/deb
-mv out-amd64/make/rpm/* out/make/rpm
-mv out-amd64/make/zip/* out/make/zip
-rm -rf out-arm64 out-amd64
+mv ./out/ ./out-amd64
 
 # Copy binaries to linux-repos
 echo ""
