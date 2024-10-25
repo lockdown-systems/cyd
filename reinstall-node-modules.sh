fee185ead626500e9f8e12bde749c73bbb0003ca
Micah Lee <micah@micahflee.com>
2024-10-25 14:47:55 -0700

    Make reinstall-node-modules.sh work for all platforms

--- reinstall-node-modules.sh
+++ reinstall-node-modules.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 rm -rf node_modules
 npm install
-sudo chown root:root node_modules/electron/dist/chrome-sandbox
-sudo chmod 4755 node_modules/electron/dist/chrome-sandbox
+
+if [ "$(uname)" == "Linux" ]; then
+    sudo chown root:root node_modules/electron/dist/chrome-sandbox
+    sudo chmod 4755 node_modules/electron/dist/chrome-sandbox
+fi
