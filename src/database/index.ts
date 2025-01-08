68665c3c86800f8ace28539ea26c474bdc905cf1
Micah Lee <micah@lockdown.systems>
2025-01-08 10:03:49 -0800

    Define IPC in database again

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,3 +7,15 @@ export * from './error_report';
 export * from './account';
 export * from './x_account';
 export * from './bluesky_account';
+
+// IPC
+
+import { defineIPCDatabaseConfig } from './config';
+import { defineIPCDatabaseErrorReport } from './error_report';
+import { defineIPCDatabaseAccount } from './account';
+
+export const defineIPCDatabase = () => {
+    defineIPCDatabaseConfig();
+    defineIPCDatabaseErrorReport();
+    defineIPCDatabaseAccount();
+}
