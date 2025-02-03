8db4d90e815d569eee4932082155829b096b5c62
Micah Lee <micah@lockdown.systems>
2025-02-03 10:35:13 -0800

    Start an interface for connecting a Bluesky account

--- src/renderer/src/views/x/XWizardMigrate.vue
+++ src/renderer/src/views/x/XWizardMigrate.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import {
     AccountXViewModel
 } from '../../view_models/AccountXViewModel'
+
+const blueskyUsername = ref('');
 
 // Props
 defineProps<{
@@ -9,6 +12,10 @@ defineProps<{
     userAuthenticated: boolean;
     userPremium: boolean;
 }>();
+
+const connectClicked = () => {
+    console.log('Connect to Bluesky');
+}
 </script>
 
 <template>
@@ -25,20 +32,34 @@ defineProps<{
             <div class="alert alert-light" role="alert">
                 <div class="small">
                     <p>
-                        <strong>Considering making a new Bluesky account just for your old tweets.</strong> This is a
-                        good way to keep your old tweets separate from your new posts.
+                        <strong>Considering making a new Bluesky account just for your old tweets.</strong><br>
+                        This is a good way to keep your old tweets separate from your new posts.
                     </p>
                     <p>
-                        <strong>Not all of your tweets can be imported into Bluesky.</strong> Cyd will import tweets
-                        that are not replies, and also tweets where you're replying to yourself, which will keep your
-                        threads intact.
+                        <strong>Not all of your tweets can be imported into Bluesky.</strong><br>
+                        Cyd will import tweets that are not replies, and also tweets where you're replying to yourself,
+                        which will keep your threads intact.
                     </p>
                 </div>
             </div>
 
-            <p>
-                asdf
-            </p>
+            <form @submit.prevent="connectClicked">
+                <p>
+                    To get started, connect your Bluesky account.
+                </p>
+                <div class="form-group">
+                    <label for="username" class="sr-only">Bluesky Username</label>
+                    <div class="input-group">
+                        <input id="username" v-model="blueskyUsername" type="text" class="form-control"
+                            placeholder="Bluesky Username" required>
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" type="submit">
+                                Connect
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </template>
