b3cccec263be9e9d92ffb74b13a14540750779ed
redshiftzero <jen@lockdown.systems>
2025-02-20 09:35:10 -0500

    facebook: start implementing `importFacebookArchive`

--- src/account_facebook/types.ts
+++ src/account_facebook/types.ts
@@ -29,3 +29,32 @@ export function convertFacebookJobRowToFacebookJob(row: FacebookJobRow): Faceboo
         error: row.error,
     };
 }
+
+export interface FacebookArchivePost {
+    // edit_info: any;
+    // retweeted: boolean;
+    // source: string;
+    // entities: XAPILegacyEntities;
+    // extended_entities: XAPILegacyEntities;
+    // display_text_range: any;
+    // favorite_count: number;
+    // in_reply_to_status_id_str: string | null;
+    // id_str: string;
+    // in_reply_to_user_id?: string;
+    // truncated: boolean;
+    // retweet_count: number;
+    // id: string;
+    // in_reply_to_status_id?: string;
+    // possibly_sensitive?: boolean;
+    created_at: string;
+    // favorited: boolean;
+    full_text: string;
+    title: string;
+    // lang: string;
+    // in_reply_to_screen_name?: string;
+    // in_reply_to_user_id_str: string | null;
+}
+
+export interface FacebookArchivePostContainer {
+    post: FacebookArchivePost;
+}
