87607870e25d76707fe1301590d4dbf676abfbc3
redshiftzero <jen@lockdown.systems>
2025-02-20 16:01:39 -0500

    facebook: add initial archive site

---
+++ archive-static-sites/facebook-archive/src/types.ts
@@ -0,0 +1,23 @@
+export type Post = {
+    postID: string;
+    createdAt: string;
+    text: string;
+    isLiked: boolean;
+    isRetweeted: boolean;
+    path: string;
+    archivedAt: string | null;
+};
+
+export type FacebookArchive = {
+    posts: Post[];
+    username: string;
+    appVersion: string;
+    createdAt: string;
+};
+
+export type User = {
+    userID: string;
+    name: string;
+    username: string;
+    profileImageDataURI: string;
+};
