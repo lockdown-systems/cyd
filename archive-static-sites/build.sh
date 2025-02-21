b4e07d84c6b0bdfb9fb3dda3cb05bf7eea7f8f64
redshiftzero <jen@lockdown.systems>
2025-02-20 20:15:37 -0500

    facebook: package up facebook-archive site
    there is a lot of duplicated code here, just getting things working

--- archive-static-sites/build.sh
+++ archive-static-sites/build.sh
@@ -3,7 +3,7 @@
 # Change to the directory of the script
 cd "$(dirname "$0")"
 
-# Build the archive static site
+# Build the archive static sites
 echo ">> Building X archive static site..."
 rm -f x-archive/public/assets/archive.js
 cd x-archive
@@ -16,4 +16,18 @@ cd dist
 mkdir -p ../../../build/
 rm -f ../../../build/x-archive.zip
 zip -r ../../../build/x-archive.zip .
+cd ../..
+
+echo ">> Building Facebook archive static site..."
+rm -f facebook-archive/public/assets/archive.js
+cd facebook-archive
+rm -r dist || true
+npm install
+npm run build
+
+# Zip it up
+cd dist
+mkdir -p ../../../build/
+rm -f ../../../build/facebook-archive.zip
+zip -r ../../../build/facebook-archive.zip .
 cd ../..
