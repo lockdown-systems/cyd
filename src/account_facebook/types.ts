cf7564445bb1ac396abca7c6a267bf8b4cf824b7
Micah Lee <micah@micahflee.com>
2025-04-24 13:17:35 -0700

    Flesh out types in much more detail

--- src/account_facebook/types.ts
+++ src/account_facebook/types.ts
@@ -2,8 +2,11 @@
 
 import { FacebookJob } from '../shared_types'
 
+// ======
 // Models
+// ======
 
+// Represents Cyd jobs
 export interface FacebookJobRow {
     id: number;
     jobType: string;
@@ -15,7 +18,100 @@ export interface FacebookJobRow {
     error: string | null;
 }
 
+// Facebook user
+export interface FacebookUserRow {
+    id: number;
+    userID: string;
+    url: string;
+    name: string;
+    // We download the profile picture and save it to the filesystem, and save the path to the file here
+    // To make unique filenames, this will be the userID + the file extension
+    profilePictureFilename: string;
+}
+
+// Facebook story
+export interface FacebookStoryRow {
+    id: number;
+    storyID: string;
+    url: string;
+    createdAt: string;
+    // node.message.text
+    text?: string;
+    // node.title.text (for messages like "{name} updated his cover photo.")
+    title?: string;
+    // if node.attachments[0].style_type_renderer.__typename is StoryAttachmentLifeEventStyleRenderer,
+    // node.attachments[0].style_type_renderer.attachment.style_infos[0].life_event_title
+    lifeEventTitle?: string;
+    // comet_sections.actor_photo.story.actors[0].id
+    // (TODO: how do we handle more than one actor?)
+    userID: string;
+    // If there's an attached_story
+    attachedStoryID?: number; // Foreign key to attachedStory.id
+    
+    // Cyd metadata
+    addedToDatabaseAt: string;
+    archivedAt: string | null;
+    deletedPostAt: string | null;
+}
+
+// Facebook attached story
+export interface FacebookAttachedStoryRow {
+    id: number;
+    storyID: string;
+    text: string;
+}
+
+// Facebook media
+export interface FacebookMediaRow {
+    id: number;
+    storyID: string;  // Foreign key to story.storyID, or to attachedStory.storyID
+    mediaType: string; // "Photo", "Video", "GenericAttachmentMedia" (for an attached reel)
+    mediaID: string;
+    // We download the media and save it to the filesystem, and save the path to the file here
+    // To make unique filenames, this will be the mediaID + the file extension
+    filename: string;
+    // If it's Photo
+    isPlayable?: boolean;
+    accessibilityCaption?: string;
+    // If it's a GenericAttachmentMedia
+    title?: string;
+    url?: string;
+}
+
+// Join table for Facebook stories and media
+export interface FacebookMediaStoryRow {
+    id: number;
+    storyID: string;  // Foreign key to story.storyID
+    mediaID: string;  // Foreign key to media.mediaID
+}
+
+// Join table for Facebook attached stories and media
+export interface FacebookMediaAttachedStoryRow {
+    id: number;
+    storyID: string;  // Foreign key to attached_story.storyID
+    mediaID: string;  // Foreign key to media.mediaID
+}
+
+// Facebook shares (links, both internal and external)
+// attachment_type: FBShortsShareAttachment
+// attachment_type: ExternalShareAttachment
+// attachment_type: StoryAttachmentShareStyleRenderer -- a link, but the URL is not in the API response
+export interface FacebookShareRow {
+    id: number;
+    storyID: string;  // Foreign key to story.storyID
+    description: string; // node.attachments[0].style_type_renderer.attachment.description.text
+    title: string; // node.attachments[0].style_type_renderer.attachment.title
+    url: string; // node.attachments[0].style_type_renderer.attachment.url
+    mediaID: string; // Foreign key to media.mediaID
+}
+
+// TODO: For attachments, when mediaType is "Video", the download URL is not displayed in the API response,
+// so we need to figure out how to get it. Same is true when mediaType is "GenericAttachmentMedia" (for an attached reel)
+// When __typename is "StoryAttachmentShareStyleRenderer", the attachment is a link, but the URL is not in the API response.
+
+// ==========
 // Converters
+// ==========
 
 export function convertFacebookJobRowToFacebookJob(row: FacebookJobRow): FacebookJob {
     return {
@@ -30,47 +126,9 @@ export function convertFacebookJobRowToFacebookJob(row: FacebookJobRow): Faceboo
     };
 }
 
-export interface FacebookUserRow {
-    id: number;
-    userID: string;
-    name: string;
-    profileURL: string;
-}
-
-export interface FacebookPostRow {
-    id: number;
-    userID: string;
-    postID: string;
-    postURL: string;
-    createdAt: string;
-    title: string;
-    text: string;
-    addedToDatabaseAt: string;
-    archivedAt: string | null;
-    deletedPostAt: string | null;
-    hasMedia: boolean;
-    isReposted: boolean;
-    repostID: string | null;
-}
-
-export interface FacebookPostMediaRow {
-    mediaId: string;
-    postId: string;  // Foreign key to post.postID
-    type: string;
-    uri: string;
-    description: string | null;
-    createdAt: string | null;
-    addedToDatabaseAt: string;
-}
-
-export interface FacebookPostURLRow {
-    urlId: string;
-    postId: string;  // Foreign key to post.postID
-    url: string;
-    addedToDatabaseAt: string;
-}
-
+// ==================
 // Facebook API types
+// ==================
 
 export interface FBAPIPrivacyRowInput {
     allow: any[];
@@ -105,7 +163,7 @@ export interface FBAPIActor {
 
 export interface FBMedia {
     __typename: string;
-    image: {
+    image?: {
         uri: string;
         height: number;
         width: number;
@@ -113,31 +171,65 @@ export interface FBMedia {
     fallback_image?: {
         uri: string;
     };
-    is_playable: boolean;
+    is_playable?: boolean;
     id: string;
-    accessibility_caption: string;
-    focus: {
+    accessibility_caption?: string;
+    focus?: {
         x: number;
         y: number;
     };
-    __isNode: string;
+    __isNode?: string;
 }
 
 export interface FBAttachment {
     style_type_renderer: {
+        // StoryAttachmentPhotoStyleRenderer: single photo
+        // StoryAttachmentAlbumStyleRenderer: multiple photos
+        // StoryAttachmentFallbackStyleRenderer: attached story (like a reel, or a link)
+        // StoryAttachmentLifeEventStyleRenderer: life event (like a birthday)
         __typename: string;
-        attachment_type: string;
+        __isStoryAttachmentStyleRendererUnion?: string;
+        is_prod_eligible?: boolean;
+        // FBShortsShareAttachment (if __typename is StoryAttachmentFallbackStyleRenderer)
+        // ExternalShareAttachment (if __typename is StoryAttachmentFallbackStyleRenderer)
+        attachment_type?: string;
         attachment: {
             title?: string;
             url?: string;
             media?: FBMedia;
+            source?: {
+                delight_ranges: any[];
+                image_ranges: any[];
+                inline_style_ranges: any[];
+                aggregated_ranges: any[];
+                ranges: any[];
+                color_ranges: any[];
+                text: string; // "Facebook", "starfewvalleywiki.com", etc.
+            };
             description?: {
+                delight_ranges: any[];
+                image_ranges: any[];
+                inline_style_ranges: any[];
+                aggregated_ranges: any[];
+                ranges: any[];
+                color_ranges: any[];
                 text: string;
             };
             all_subattachments?: {
                 count: number;
                 nodes: { media: FBMedia; }[];
             };
+            // on StoryAttachmentLifeEventStyleRenderer
+            actor?: any;
+            subattachments?: any[];
+            style_infos?: {
+                __typename: string;
+                animated_icon_url?: string;
+                icon_id?: string;
+                icon_url?: string;
+                life_event_title?: string; // "Born on February 12, 1983"
+                third_person_title?: string; // "Chase Westbrook Was Born on February 12, 1983"
+            }[];
         };
         __module_operation_ProfileCometTimelineGridStoryAttachment_story: any;
         __module_component_ProfileCometTimelineGridStoryAttachment_story: any;
@@ -259,32 +351,7 @@ export interface FBAPINode {
     __isNode: string;
 }
 
-export interface FBAPIResponse {
-    label?: string;
-    path?: (string | number)[];
-    data?: {
-        user?: {
-            timeline_manage_feed_units: {
-                edges: {
-                    node: FBAPINode,
-                    cursor: string;
-                }[];
-            };
-            id: string;
-            profile_pinned_post: any;
-        };
-        node?: FBAPINode;
-        page_info?: {
-            end_cursor: string;
-            has_next_page: boolean;
-        }
-    };
-    extensions?: any;
-}
-
-// =====================================
 // Manage Posts types that we care about
-// =====================================
 
 // ProfileCometManagePostsTimelineRootQuery types:
 // - FBAPIResponseProfileCometManagePostsTimelineRootQuery1
