fce90a746d2d3f3fbfff19cd4ad5c007d5d381d3
Micah Lee <micah@micahflee.com>
2025-06-29 13:56:18 -0700

    Update build options and import pages

--- src/renderer/src/views/x/XWizardBuildOptionsPage.vue
+++ src/renderer/src/views/x/XWizardBuildOptionsPage.vue
@@ -81,77 +81,82 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="wizard-content container mb-4 mt-3 mx-auto">
-        <div class="mb-4">
-            <h2>
-                Build options
-            </h2>
-            <p class="text-muted">
-                You can save tweets, likes, and direct messages.
-            </p>
+    <div class="wizard-content">
+        <div class="back-buttons">
+            <button type="submit" class="btn btn-outline-secondary text-nowrap m-1" @click="backClicked">
+                <i class="fa-solid fa-backward" />
+                Back to Import or Build Database
+            </button>
         </div>
 
-        <XLastImportOrBuildComponent :account-i-d="model.account.id" :show-button="false" :show-no-data-warning="false"
-            @set-state="emit('setState', $event)" />
-
-        <form @submit.prevent>
-            <div class="mb-3">
-                <div class="form-check">
-                    <input id="archiveTweets" v-model="archiveTweets" type="checkbox" class="form-check-input">
-                    <label class="form-check-label" for="archiveTweets">Save my tweets</label>
-                </div>
+        <div class="wizard-scroll-content">
+            <div class="mb-4">
+                <h2>
+                    Build options
+                </h2>
+                <p class="text-muted">
+                    You can save tweets, likes, and direct messages.
+                </p>
             </div>
-            <div class="indent">
+
+            <XLastImportOrBuildComponent :account-i-d="model.account.id" :show-button="false"
+                :show-no-data-warning="false" @set-state="emit('setState', $event)" />
+
+            <form @submit.prevent>
                 <div class="mb-3">
                     <div class="form-check">
-                        <input id="archiveTweetsHTML" v-model="archiveTweetsHTML" type="checkbox"
-                            class="form-check-input" :disabled="!archiveTweets">
-                        <label class="form-check-label" for="archiveTweetsHTML">
-                            Save an HTML version of each tweet
-                        </label>
-                    </div>
-                    <div class="indent">
-                        <small class="form-text text-muted">
-                            Make an HTML archive of each tweet, including its replies, which is good
-                            for
-                            taking screenshots
-                            <em>(takes longer)</em>
-                        </small>
+                        <input id="archiveTweets" v-model="archiveTweets" type="checkbox" class="form-check-input">
+                        <label class="form-check-label" for="archiveTweets">Save my tweets</label>
                     </div>
                 </div>
-            </div>
-            <div class="mb-3">
-                <div class="form-check">
-                    <input id="archiveLikes" v-model="archiveLikes" type="checkbox" class="form-check-input">
-                    <label class="form-check-label" for="archiveLikes">Save my likes</label>
+                <div class="indent">
+                    <div class="mb-3">
+                        <div class="form-check">
+                            <input id="archiveTweetsHTML" v-model="archiveTweetsHTML" type="checkbox"
+                                class="form-check-input" :disabled="!archiveTweets">
+                            <label class="form-check-label" for="archiveTweetsHTML">
+                                Save an HTML version of each tweet
+                            </label>
+                        </div>
+                        <div class="indent">
+                            <small class="form-text text-muted">
+                                Make an HTML archive of each tweet, including its replies, which is good
+                                for
+                                taking screenshots
+                                <em>(takes longer)</em>
+                            </small>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="mb-3">
-                <div class="form-check">
-                    <input id="archiveBookmarks" v-model="archiveBookmarks" type="checkbox" class="form-check-input">
-                    <label class="form-check-label" for="archiveBookmarks">Save my bookmarks</label>
+                <div class="mb-3">
+                    <div class="form-check">
+                        <input id="archiveLikes" v-model="archiveLikes" type="checkbox" class="form-check-input">
+                        <label class="form-check-label" for="archiveLikes">Save my likes</label>
+                    </div>
                 </div>
-            </div>
-            <div class="mb-3">
-                <div class="form-check">
-                    <input id="archiveDMs" v-model="archiveDMs" type="checkbox" class="form-check-input">
-                    <label class="form-check-label" for="archiveDMs">Save my direct messages</label>
+                <div class="mb-3">
+                    <div class="form-check">
+                        <input id="archiveBookmarks" v-model="archiveBookmarks" type="checkbox"
+                            class="form-check-input">
+                        <label class="form-check-label" for="archiveBookmarks">Save my bookmarks</label>
+                    </div>
                 </div>
-            </div>
+                <div class="mb-3">
+                    <div class="form-check">
+                        <input id="archiveDMs" v-model="archiveDMs" type="checkbox" class="form-check-input">
+                        <label class="form-check-label" for="archiveDMs">Save my direct messages</label>
+                    </div>
+                </div>
+            </form>
+        </div>
 
-            <div class="buttons">
-                <button type="submit" class="btn btn-outline-secondary text-nowrap m-1" @click="backClicked">
-                    <i class="fa-solid fa-backward" />
-                    Back to Import or Build Database
-                </button>
-
-                <button type="submit" class="btn btn-primary text-nowrap m-1"
-                    :disabled="!(archiveTweets || archiveLikes || archiveBookmarks || archiveDMs)" @click="nextClicked">
-                    <i class="fa-solid fa-forward" />
-                    Continue to Review
-                </button>
-            </div>
-        </form>
+        <div class="next-buttons">
+            <button type="submit" class="btn btn-primary text-nowrap m-1"
+                :disabled="!(archiveTweets || archiveLikes || archiveBookmarks || archiveDMs)" @click="nextClicked">
+                <i class="fa-solid fa-forward" />
+                Continue to Review
+            </button>
+        </div>
     </div>
 </template>
 
--- src/renderer/src/views/x/XWizardImportPage.vue
+++ src/renderer/src/views/x/XWizardImportPage.vue
@@ -29,58 +29,61 @@ const backClicked = async () => {
 </script>
 
 <template>
-    <div class="wizard-content container mb-4 mt-3 mx-auto">
-        <div class="mb-4">
-            <h2>
-                Import your X archive
-            </h2>
-            <p class="text-muted">
-                Before you can import your X archive, you need to download it by following these steps:
-            </p>
-            <ul class="x-archive-steps">
-                <li>
-                    <strong>Visit <a href="#" @click="openURL('https://x.com/settings/download_your_data')">
-                            https://x.com/settings/download_your_data</a>.</strong><br>
-                    <small class="text-muted">You might need to sign in to X first.</small>
-                </li>
-                <li>
-                    <strong>Prove your identity.</strong><br>
-                    <small class="text-muted">You'll probably need to type your X password. You might also need to get
-                        a verification code sent to your email, or do other things to verify your identity.</small>
-                </li>
-                <li>
-                    <strong>Click the "Request archive" button.</strong>
-                </li>
-                <li>
-                    <strong>Be patient.</strong><br>
-                    <small class="text-muted">After requesting your archive, you'll need to wait <strong>at least a
-                            day</strong>, and maybe longer. Sorry!</small>
-                </li>
-                <li>
-                    <strong>When it's ready, download the ZIP file from X.</strong><br>
-                    <small class="text-muted">After you've followed these steps and you have your archive ZIP file,
-                        click the button below.</small>
-                </li>
-            </ul>
+    <div class="wizard-content">
+        <div class="back-buttons">
+            <button type="submit" class="btn btn-outline-secondary text-nowrap m-1" @click="backClicked">
+                <i class="fa-solid fa-backward" />
+                Back to Import or Build Database
+            </button>
+        </div>
 
-            <div class="buttons mb-4">
-                <button type="submit" class="btn btn-primary text-nowrap m-1" :disabled="!(
-                    model.account?.xAccount?.archiveTweets ||
-                    model.account?.xAccount?.archiveLikes ||
-                    model.account?.xAccount?.archiveDMs)" @click="importClicked">
-                    <i class="fa-solid fa-file-import" />
-                    I've Downloaded My Archive from X
-                </button>
+        <div class="wizard-scroll-content">
+            <div class="mb-4">
+                <h2>
+                    Import your X archive
+                </h2>
+                <p class="text-muted">
+                    Before you can import your X archive, you need to download it by following these steps:
+                </p>
+                <ul class="x-archive-steps">
+                    <li>
+                        <strong>Visit <a href="#" @click="openURL('https://x.com/settings/download_your_data')">
+                                https://x.com/settings/download_your_data</a>.</strong><br>
+                        <small class="text-muted">You might need to sign in to X first.</small>
+                    </li>
+                    <li>
+                        <strong>Prove your identity.</strong><br>
+                        <small class="text-muted">You'll probably need to type your X password. You might also need to
+                            get
+                            a verification code sent to your email, or do other things to verify your identity.</small>
+                    </li>
+                    <li>
+                        <strong>Click the "Request archive" button.</strong>
+                    </li>
+                    <li>
+                        <strong>Be patient.</strong><br>
+                        <small class="text-muted">After requesting your archive, you'll need to wait <strong>at least a
+                                day</strong>, and maybe longer. Sorry!</small>
+                    </li>
+                    <li>
+                        <strong>When it's ready, download the ZIP file from X.</strong><br>
+                        <small class="text-muted">After you've followed these steps and you have your archive ZIP file,
+                            click the button below.</small>
+                    </li>
+                </ul>
 
-                <button type="submit" class="btn btn-outline-secondary text-nowrap m-1" @click="backClicked">
-                    <i class="fa-solid fa-backward" />
-                    Back to Import or Build Database
-                </button>
+                <XLastImportOrBuildComponent :account-i-d="model.account.id" :show-button="false"
+                    :show-no-data-warning="false" @set-state="emit('setState', $event)" />
             </div>
-
-            <XLastImportOrBuildComponent :account-i-d="model.account.id" :button-text="'Go to Delete Options'"
-                :button-text-no-data="'Skip to Delete Options'" :button-state="State.WizardDeleteOptions"
-                @set-state="emit('setState', $event)" />
+        </div>
+        <div class="next-buttons">
+            <button type="submit" class="btn btn-primary text-nowrap m-1" :disabled="!(
+                model.account?.xAccount?.archiveTweets ||
+                model.account?.xAccount?.archiveLikes ||
+                model.account?.xAccount?.archiveDMs)" @click="importClicked">
+                <i class="fa-solid fa-file-import" />
+                I've Downloaded My Archive from X
+            </button>
         </div>
     </div>
 </template>
