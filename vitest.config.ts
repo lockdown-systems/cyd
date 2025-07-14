3eb13c815d26096f04e001a53f61977652e1bdbd
Micah Lee <micah@micahflee.com>
2025-07-14 16:58:43 -0700

    Start porting SignInModal tests to vitest, and refactor the package.json test script

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  plugins: [vue()],
   test: {
     environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     coverage: {
       provider: "v8",
       exclude: [
