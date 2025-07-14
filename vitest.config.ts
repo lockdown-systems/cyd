6f122a506f6e82de2076316630a6b448ae3d8d5a
Micah Lee <micah@micahflee.com>
2025-07-14 16:40:23 -0700

    Show test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,25 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      exclude: [
+        "coverage/**",
+        "dist/**",
+        ".vite/**",
+        "node_modules/**",
+        "*.ts",
+        "**/*.d.ts",
+        "archive-static-sites/**",
+        "docs/**",
+        "scripts/**",
+        "build/**",
+        "assets/**",
+        "testdata/**",
+        "forge.config.ts",
+        "vite.*.config.ts",
+        "eslint.config.mjs",
+      ],
+    },
   },
 });
