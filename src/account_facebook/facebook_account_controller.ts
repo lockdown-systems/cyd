7e620f4118247021d1b5731fb21c9e7b53333554
redshiftzero <jen@lockdown.systems>
2025-11-02 16:11:03 -0500

    refactor: move migrations for Facebook to own file

--- src/account_facebook/facebook_account_controller.ts
+++ src/account_facebook/facebook_account_controller.ts
@@ -44,6 +44,7 @@ import {
 
 // for building the static archive site
 import { saveArchive } from "./archive";
+import { migrations } from "./controller/migrations";
 
 function getURLFileExtension(urlString: string) {
   const url = new URL(urlString);
@@ -141,82 +142,7 @@ export class FacebookAccountController extends BaseAccountController<FacebookPro
     log.info(`FacebookAccountController.initDB: dbPath=${dbPath}`);
     this.db = new Database(dbPath, {});
     this.db.pragma("journal_mode = WAL");
-    runMigrations(this.db, [
-      // Create the tables
-      {
-        name: "initial",
-        sql: [
-          `CREATE TABLE job (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    jobType TEXT NOT NULL,
-    status TEXT NOT NULL,
-    scheduledAt DATETIME NOT NULL,
-    startedAt DATETIME,
-    finishedAt DATETIME,
-    progressJSON TEXT,
-    error TEXT
-);`,
-          `CREATE TABLE config (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    key TEXT NOT NULL UNIQUE,
-    value TEXT NOT NULL
-);`,
-          `CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    userID TEXT NOT NULL UNIQUE,
-    url TEXT NOT NULL,
-    name TEXT NOT NULL,
-    profilePictureFilename TEXT NOT NULL
-);`,
-          `CREATE TABLE story (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    storyID TEXT NOT NULL UNIQUE,
-    url TEXT NOT NULL,
-    createdAt DATETIME NOT NULL,
-    text TEXT,
-    title TEXT,
-    lifeEventTitle TEXT,
-    userID TEXT NOT NULL, -- Foreign key to user.userID
-    attachedStoryID INTEGER, -- Foreign key to attached_story.id
-    addedToDatabaseAt DATETIME NOT NULL,
-    archivedAt DATETIME,
-    deletedStoryAt DATETIME,
-    FOREIGN KEY(userID) REFERENCES user(userID),
-    FOREIGN KEY(attachedStoryID) REFERENCES attached_story(storyID)
-);`,
-          `CREATE TABLE attached_story (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    storyID TEXT NOT NULL UNIQUE,
-    text TEXT
-);`,
-          `CREATE TABLE media (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    mediaType TEXT NOT NULL, -- "Photo", "Video", "GenericAttachmentMedia"
-    mediaID TEXT NOT NULL UNIQUE,
-    filename TEXT,
-    isPlayable BOOLEAN,
-    accessibilityCaption TEXT,
-                        title TEXT,
-    url TEXT,
-    needsVideoDownload BOOLEAN DEFAULT 0
-);`,
-          `CREATE TABLE media_story (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    storyID TEXT NOT NULL, -- Foreign key to story.storyID
-    mediaID TEXT NOT NULL, -- Foreign key to media.mediaID
-    FOREIGN KEY(storyID) REFERENCES story(storyID),
-    FOREIGN KEY(mediaID) REFERENCES media(mediaID)
-);`,
-          `CREATE TABLE media_attached_story (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    storyID TEXT NOT NULL, -- Foreign key to attached_story.storyID
-    mediaID TEXT NOT NULL, -- Foreign key to media.mediaID
-    FOREIGN KEY(storyID) REFERENCES attached_story(storyID),
-    FOREIGN KEY(mediaID) REFERENCES media(mediaID)
-);`,
-        ],
-      },
-    ]);
+    runMigrations(this.db, migrations);
     log.info("FacebookAccountController.initDB: database initialized");
   }
 
