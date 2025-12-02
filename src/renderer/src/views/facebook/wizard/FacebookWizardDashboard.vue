9071547ad042d0b14497ba8ae168cae8b14fdc27
Micah Lee <micah@micahflee.com>
2025-12-01 19:00:36 -0800

    Move dashboard styles into a global style.css, and also move the App.vue global styles there too

--- src/renderer/src/views/facebook/wizard/FacebookWizardDashboard.vue
+++ src/renderer/src/views/facebook/wizard/FacebookWizardDashboard.vue
@@ -9,8 +9,6 @@ const props = defineProps<{
   model: FacebookViewModel;
 }>();
 
-const facebookAccount = computed(() => props.model.account.facebookAccount);
-
 const cards = computed(() => [
   {
     icon: "/assets/icon-database.png",
@@ -48,34 +46,3 @@ const cards = computed(() => [
     </div>
   </div>
 </template>
-
-<style scoped>
-.wizard-scroll-content {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 100%;
-  height: 100%;
-  padding: 1rem 2rem;
-}
-
-.dashboard {
-  width: 100%;
-}
-
-.card img {
-  height: 80px;
-  width: auto;
-}
-
-.card h2 {
-  margin-top: 1rem;
-  font-size: 1.5rem;
-}
-
-.disabled-card {
-  cursor: default;
-  opacity: 0.9;
-}
-</style>
