b4063267089bd1302669afdd807ade2770bd102c
Micah Lee <micah@micahflee.com>
2026-03-27 09:27:00 -0700

    Remove docs

--- docs/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// eslint-disable-next-line import/no-unresolved
-import { Redirect } from "@docusaurus/router";
-
-export default function Home() {
-  return <Redirect to="/docs/intro/" />;
-}
