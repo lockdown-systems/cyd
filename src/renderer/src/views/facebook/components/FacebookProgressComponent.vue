f2a30047412bf646ef1e0783c2b1c6b995cb870d
redshiftzero <jen@lockdown.systems>
2026-06-21 17:42:44 -0400

    facebook: update UI elements in `FacebookProgressComponent`

--- src/renderer/src/views/facebook/components/FacebookProgressComponent.vue
+++ src/renderer/src/views/facebook/components/FacebookProgressComponent.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
-import { marked } from "marked";
 import type { FacebookProgress } from "../../../view_models/FacebookViewModel/types";
+import { FACEBOOK_DELETE_CATEGORIES } from "../../../view_models/FacebookViewModel/categories";
 
 const { t } = useI18n();
 
@@ -10,26 +10,28 @@ const props = defineProps<{
   progress: FacebookProgress | null;
 }>();
 
-const wallPostsProgressHtml = computed(() => {
-  if (!props.progress) return "";
-  return marked.parse(
-    t("viewModels.facebook.progress.wallPostsProgress", {
-      deleteCount: props.progress.wallPostsDeleted.toLocaleString(),
-      untagCount: props.progress.wallPostsUntagged.toLocaleString(),
-      hideCount: props.progress.wallPostsHidden.toLocaleString(),
-    }),
-  );
+// One line per category that has had at least one item deleted.
+const deletedCounts = computed(() => {
+  if (!props.progress) return [];
+  return FACEBOOK_DELETE_CATEGORIES.map((category) => ({
+    setting: category.setting,
+    count: props.progress ? props.progress[category.counter] : 0,
+    label: t(`facebook.finished.${category.counter}`),
+  })).filter((entry) => entry.count > 0);
 });
 </script>
 
 <template>
   <template v-if="progress">
     <div class="progress-wrapper">
-      <!-- Delete wall posts -->
-      <template v-if="progress.currentJob == 'deleteWallPosts'">
-        <!-- eslint-disable-next-line vue/no-v-html -->
-        <div v-html="wallPostsProgressHtml" />
-        <p v-if="progress.isDeleteWallPostsFinished">
+      <!-- Delete activity -->
+      <template v-if="progress.currentJob == 'deleteActivity'">
+        <ul>
+          <li v-for="entry in deletedCounts" :key="entry.setting">
+            {{ entry.count.toLocaleString() }} {{ entry.label }}
+          </li>
+        </ul>
+        <p v-if="progress.isDeleteActivityFinished">
           {{ t("progress.savingComplete") }}
         </p>
       </template>
